6931cb8e680714f2563c6d5feaf0aaa166f0a22e
Pavlo Zin <pavlo.zin@icloud.com>
2021-03-14 22:05:39 +0200

    Implemented opening user details by clicking on the mention in timeline

--- lib/ui/timeline_list_tile.dart
+++ lib/ui/timeline_list_tile.dart
@@ -38,128 +38,119 @@ class TimelineListTile extends StatelessWidget {
                     : Hero(
                         tag: 'timelineProfilePic$index',
                         child: NetworkAvatar(url: post.userInfo.picUrl))),
-            title: Transform.translate(
-              offset: Offset(-8, 0),
-              child: Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Flexible(
-                      flex: 0,
-                      child: Text(post.userInfo.displayName,
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                          style: TextStyle(fontWeight: FontWeight.w600)),
+            title: Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Flexible(
+                    flex: 0,
+                    child: Text(post.userInfo.displayName,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: TextStyle(fontWeight: FontWeight.w600)),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      padding: EdgeInsets.only(left: 4),
+                      child: Text(
+                        "@${post.userInfo.username}",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context)
+                            .textTheme
+                            .subtitle1
+                            .copyWith(color: Theme.of(context).hintColor),
+                      ),
                     ),
-                    Flexible(
-                      flex: 1,
-                      child: Container(
-                        padding: EdgeInsets.only(left: 4),
-                        child: Text(
-                          "@${post.userInfo.username}",
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
+                  ),
+                  Expanded(
+                    flex: 0,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 4),
+                      child: Text("·",
                           style: Theme.of(context)
                               .textTheme
                               .subtitle1
-                              .copyWith(color: Theme.of(context).hintColor),
-                        ),
-                      ),
+                              .copyWith(
+                                  color: Theme.of(context).hintColor,
+                                  fontWeight: FontWeight.w500)),
                     ),
-                    Expanded(
-                      flex: 0,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 4),
-                        child: Text("·",
-                            style: Theme.of(context)
-                                .textTheme
-                                .subtitle1
-                                .copyWith(
-                                    color: Theme.of(context).hintColor,
-                                    fontWeight: FontWeight.w500)),
-                      ),
-                    ),
-                    StreamBuilder<Object>(
-                        stream: refreshTimeStream,
-                        builder: (context, snapshot) {
-                          return Expanded(
-                            flex: 0,
-                            child: Text(_humanizeTime(post.timestamp),
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .subtitle2
-                                    .copyWith(
-                                        fontWeight: FontWeight.normal,
-                                        color: Theme.of(context).hintColor)),
-                          );
-                        }),
-                  ],
-                ),
-              ),
-            ),
-            subtitle: Transform.translate(
-              offset: Offset(-8, 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                      padding: const EdgeInsets.only(top: 3.0),
-                      child: _buildPostBody(context)),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      _buildPostIcon(Feather.message_circle, "13", () {},
-                          padding:
-                              EdgeInsets.only(top: 8, right: 12, bottom: 8)),
-                      _buildPostIcon(Feather.heart, "69", () {}),
-                      _buildPostIcon(Feather.share, "", () {}),
-                    ],
-                  )
+                  ),
+                  StreamBuilder<Object>(
+                      stream: refreshTimeStream,
+                      builder: (context, snapshot) {
+                        return Expanded(
+                          flex: 0,
+                          child: Text(_humanizeTime(post.timestamp),
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .subtitle2
+                                  .copyWith(
+                                      fontWeight: FontWeight.normal,
+                                      color: Theme.of(context).hintColor)),
+                        );
+                      }),
                 ],
               ),
-            )),
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                    padding: const EdgeInsets.only(top: 3.0),
+                    child: PostBody(post: post)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    PostIcon(
+                        context: context,
+                        icon: Feather.message_circle,
+                        padding:
+                            EdgeInsets.only(top: 8, right: 12, bottom: 8),
+                        text: '13',
+                        onPressed: () {}),
+                    PostIcon(
+                        context: context,
+                        icon: Feather.heart,
+                        text: '69',
+                        onPressed: () {}),
+                    PostIcon(
+                        context: context,
+                        icon: Feather.share,
+                        onPressed: () {})
+                  ],
+                )
+              ],
+            )
+        ),
         Divider(height: 0)
       ],
     );
   }
+}
 
-  _buildPostBody(BuildContext context) {
-    return ParsedText(
-        text: post.postText,
-        style: Theme.of(context).textTheme.subtitle1,
-        parse: [
-          MatchText(
-              renderWidget: ({String text, String pattern}) {
-                return RawMaterialButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(4.0),
-                    ),
-                    constraints: BoxConstraints(minWidth: 0, minHeight: 0),
-                    padding: EdgeInsets.zero,
-                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                    onPressed: () {
-                        Navigator.pushNamed(context, '/user_details',
-                            arguments: UserDetailsArgs(
-                                userIsKnown: false,
-                                username: text,
-                                profilePicHeroTag: ''));
-                    },
-                    child: Text(text,
-                        style: Theme.of(context).textTheme.subtitle1.copyWith(
-                            color: Theme.of(context).primaryColorDark)));
-              },
-              pattern: r'@[a-zA-Z0-9][a-zA-Z0-9_.]+[a-zA-Z0-9]',
-              regexOptions: RegexOptions(
-                  multiLine: false,
-                  caseSensitive: false,
-                  unicode: false,
-                  dotAll: false))
-        ]);
-  }
+class PostIcon extends StatelessWidget {
+  final EdgeInsets padding;
+  final IconData icon;
+  final String text;
+  final Function onPressed;
 
-  _buildPostIcon(IconData icon, String text, VoidCallback onPressed,
-      {EdgeInsets padding}) {
+  const PostIcon({
+    Key key,
+    @required this.context,
+    this.padding =
+        const EdgeInsets.only(top: 8, right: 12, bottom: 8, left: 12),
+    this.icon,
+    this.text = '',
+    this.onPressed,
+  }) : super(key: key);
+
+  final BuildContext context;
+
+  @override
+  Widget build(BuildContext context) {
     return RawMaterialButton(
       materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
       constraints: BoxConstraints(minHeight: 0, minWidth: 0),
@@ -181,14 +172,58 @@ class TimelineListTile extends StatelessWidget {
       ),
     );
   }
+}
 
-  // todo refactor this
-  // todo also display actual date after some long time (eg. after 12h)
-  _humanizeTime(DateTime time) {
-    return timeago
-        .format(time, locale: 'en_short')
-        .replaceAll(RegExp("~"), '')
-        .replaceAll(RegExp("min"), 'm')
-        .replaceAll(RegExp(" "), '');
+class PostBody extends StatelessWidget {
+  final Post post;
+
+  const PostBody({
+    Key key,
+    @required this.post,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ParsedText(
+        text: post.postText,
+        style: Theme.of(context).textTheme.subtitle1,
+        parse: [
+          MatchText(
+              renderWidget: ({String text, String pattern}) {
+                return RawMaterialButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                    constraints: BoxConstraints(minWidth: 0, minHeight: 0),
+                    padding: EdgeInsets.zero,
+                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/user_details',
+                          arguments: UserDetailsArgs(
+                              userIsKnown: false,
+                              username: text,
+                              profilePicHeroTag: ''));
+                    },
+                    child: Text(text,
+                        style: Theme.of(context).textTheme.subtitle1.copyWith(
+                            color: Theme.of(context).primaryColorDark)));
+              },
+              pattern: r'@[a-zA-Z0-9][a-zA-Z0-9_.]+[a-zA-Z0-9]',
+              regexOptions: RegexOptions(
+                  multiLine: false,
+                  caseSensitive: false,
+                  unicode: false,
+                  dotAll: false))
+        ]);
   }
+}
+
+// todo refactor this
+// todo also display actual date after some long time (eg. after 12h)
+_humanizeTime(DateTime time) {
+  return timeago
+      .format(time, locale: 'en_short')
+      .replaceAll(RegExp("~"), '')
+      .replaceAll(RegExp("min"), 'm')
+      .replaceAll(RegExp(" "), '');
 }
